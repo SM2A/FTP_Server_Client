e78364eb011c811a796dd1ede59e845e05e46ffe
SM2A <seyedamin1990@gmail.com>
2022-03-23 12:49:35 +0430

    test

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# FTP_Server_Client
+# FTP Server Client
 Computer Networks Course Computer Assignment 1
---
+++ Source/Client/test.h
@@ -0,0 +1,10 @@
+//
+// Created by amin on 3/23/22.
+//
+
+#ifndef FTP_SERVER_CLIENT_TEST_H
+#define FTP_SERVER_CLIENT_TEST_H
+
+std::string name = "asdasdasdasda";
+
+#endif //FTP_SERVER_CLIENT_TEST_H
