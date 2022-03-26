7064fedd27f08b80e24cbe9caaea06423056fab1
SM2A <seyedamin1990@gmail.com>
2022-03-26 13:52:03 +0430

    Test yml

--- Source/Server/main.cpp
+++ Source/Server/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 #include "Server.h"
+#include "CommandExecutor.h"
 
 using namespace std;
 
 int main(int argc, char **argv) {
     try {
+        cout<<CommandExecutor::pwd()<<endl;
         if (argc == 2) Server::getInstance()->init(string(argv[1]));
         else if (argc < 2) throw invalid_argument("No file entered");
         else if (argc > 2) throw invalid_argument("Fix arguments");
